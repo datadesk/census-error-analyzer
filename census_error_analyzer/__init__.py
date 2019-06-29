7c3746b346afe2a179eb0aff4a431059a249c60a
Natarajan Krishnaswami <natarajan@krishnaswami.org>
2019-06-29 10:24:59 -0400

    Add functions for calculating derived estimates and their MOEs from https://www.census.gov/content/dam/Census/library/publications/2018/acs/acs_general_handbook_2018_ch08.pdf

--- census_error_analyzer/__init__.py
+++ census_error_analyzer/__init__.py
@@ -120,3 +120,136 @@ def is_statistically_different(pair_one, pair_two):
         True
     """
     return statistical_difference(pair_one, pair_two) > 1.0
+
+
+def aggregate_counts(pairs):
+    """
+    Computes a derived estimate and its MOE for the aggregation of all
+    the arguments.
+
+    Args:
+        pairs (list): a list of two-item sequences with a U.S. Census bureau
+            count estimate and its margin of error.
+
+    Returns:
+        A two-item sequence containing the aggregated estimate and its MOE.
+
+    Examples:
+        >>> aggregate_counts([(379, 1), (384, 1)])
+        (763, 1.4142135623730953)
+
+    """
+    estimate = 0
+    squared_moe = 0
+    
+    # Pull out the values
+    for (value, moe) in pairs:
+        estimate += value
+        squared_moe += moe**2
+
+    return (estimate, math.sqrt(squared_moe))
+
+
+def calculate_proportion(numerator_pair, denominator_pair):
+    """
+    Computes a derived estimate and its MOE for the proportion between
+    the numerator and denominator arguments where the numerator
+    represents a subset of the denominator.
+
+    Args:
+        numerator (list): a two-item sequence with a U.S. Census
+            bureau estimate and its margin of error.
+
+        denominator (list): a two-item sequence with a U.S. Census
+            bureau estimate and its margin of error.
+
+    Returns:
+        A two-item sequence containing the calculated estimate and its MOE.
+
+    Examples:
+        >>> calculate_proportion((379, 1), (384, 1))
+        (0.9869791666666666, 0.02046644492870734)
+
+    """
+    # Pull out the values
+    numerator_estimate, numerator_moe = numerator_pair
+    denominator_estimate, denominator_moe = denominator_pair
+
+    # Calculate the proportion
+    proportion_estimate = numerator_estimate / denominator_estimate
+
+    # Calculate the proportion MOE
+    squared_proportion_moe = (
+        numerator_moe**2 - proportion_estimate**2 * denominator_moe**2
+    ) / denominator_estimate
+
+    return (proportion_estimate, math.sqrt(squared_proportion_moe))
+
+
+def calculate_ratio(numerator_pair, denominator_pair):
+    """
+    Computes a derived estimate and its MOE for the ratio between
+    the numerator and denominator arguments where the numerator
+    does not represent a subset of the denominator.
+
+    Args:
+        numerator (list): a two-item sequence with a U.S. Census
+            bureau estimate and its margin of error.
+
+        denominator (list): a two-item sequence with a U.S. Census
+            bureau estimate and its margin of error.
+
+    Returns:
+        A two-item sequence containing the calculated estimate and its MOE.
+
+    Examples:
+        >>> calculate_ratio((379, 1), (384, 1))
+        (0.9869791666666666, 0.0604892511542549)
+    """
+    # Pull out the values
+    numerator_estimate, numerator_moe = numerator_pair
+    denominator_estimate, denominator_moe = denominator_pair
+
+    # Calculate the ratio
+    ratio_estimate = numerator_estimate / denominator_estimate
+
+    # Calculate the ratio MOE
+    squared_ratio_moe = (
+        numerator_moe**2 + ratio_estimate**2 * denominator_moe**2
+    ) / denominator_estimate
+
+    return (ratio_estimate, math.sqrt(squared_ratio_moe))
+
+
+def calculate_product(pair_one, pair_two):
+    """
+    Computes a derived estimate and its MOE for the product between
+    the arguments.
+
+    Args:
+        pair_one (list): a two-item sequence with a U.S. Census
+            bureau estimate and its margin of error.
+
+        pair_two (list): a two-item sequence with a U.S. Census
+            bureau estimate and its margin of error.
+
+    Returns:
+        A two-item sequence containing the calculated estimate and its MOE.
+
+    Examples:
+        >>> calculate_product((384, 1), (0.987, 0.06))
+        (379.008, 23.061131130107213)
+        
+    """
+    # Pull out the values
+    estimate_one, moe_one = pair_one
+    estimate_two, moe_two = pair_two
+
+    # Calculate the product
+    product_estimate = estimate_one * estimate_two
+    
+    # Calculate the product MOE
+    squared_product_moe = (estimate_one**2 * moe_two**2 +
+                           estimate_two**2 * moe_one**2)
+
+    return (products_estimate, math.sqrt(squared_product_moe))
